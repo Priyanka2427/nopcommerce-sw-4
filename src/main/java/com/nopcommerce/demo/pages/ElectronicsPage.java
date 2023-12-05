c94b8966716f616000a80ad2d538be60f1561ac2
priyankagajera <prgajera2427@gmail.com>
2023-12-05 18:48:58 +0000

    My homework

---
+++ src/main/java/com/nopcommerce/demo/pages/ElectronicsPage.java
@@ -0,0 +1,16 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class ElectronicsPage extends Utility {
+    /******************************** All Elements' Path on Electronics page ***********************************************/
+
+    By welcomeText = By.xpath("//h1[normalize-space()='Electronics']");
+
+    /******************************** All Elements' Action Methods on Electronics page *************************************/
+
+    public String getWelcomeText() {
+        return getTextFromElement(welcomeText);
+    }
+}
